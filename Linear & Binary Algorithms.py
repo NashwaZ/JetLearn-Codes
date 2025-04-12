6ff23688fb6e926397ef3fcc14512c5d2f4fb6d3
NashwaZ <nashwa.zshan@gmail.com>
2025-04-12 20:20:37 +0400

    Add files via upload

--- Linear & Binary Algorithms.py
+++ Linear & Binary Algorithms.py
@@ -1,12 +1,14 @@
+'''
+
 list = [21,34,67,2,54,1,34,7,3,87,45,8,4]
 
 key = int(input("Enter no. to check: "))
-'''
+
 if key in list:
     print("yes, number is present")
 else:
     print("number is not present")
-'''
+
 for l in list:
     if l == key:
         print("number is in list")
@@ -30,3 +32,29 @@ while low <= high:
         high = middle - 1
 else:
     print("number is not present")
+
+'''
+    
+#12/04/2025 Binary Search HW
+
+sorted_list = [1, 4, 10, 12, 54, 67, 77, 79, 87, 90, 93, 99, 101, 231]
+
+key = int(input("Enter number: "))
+
+low = 0
+high = len(sorted_list) - 1
+
+while low <= high:
+    middle = (low + high) // 2
+    if sorted_list[middle] == key:
+        print(f"{key} is in the list at index {middle}")
+        break
+    elif sorted_list[middle] < key:
+        low = middle + 1
+    else:
+        high = middle - 1
+else:
+    print("Number is not present in the list")
+    
+
+    
