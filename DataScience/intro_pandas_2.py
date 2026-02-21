5b9dccbd99bc05aadc731310f7feb25f91112bb6
NashwaZ <nashwa.zshan@gmail.com>
2026-02-21 20:58:29 +0400

    Add files via upload

--- DataScience/intro_pandas_2.py
+++ DataScience/intro_pandas_2.py
@@ -26,3 +26,36 @@ print(new_df.info())
 #Arthmetic operations on one or multiple columns
 print(titanic_df['Age'].mean())
 print(titanic_df[['Age','Fare']].mean())
+
+#Group data by categories
+#Grouping data by sex and calculating mean of age
+data_by_sex = titanic_df[['Sex','Age']].groupby('Sex').mean()
+print(data_by_sex)
+
+#Mean ticket price for each sex and passenger class combined
+data_by_class = titanic_df.groupby(['Sex','Pclass'])['Fare'].mean()
+print(data_by_class)
+
+#Counting no. of rows in each passenger category
+data_no_class = titanic_df['Pclass'].value_counts()
+print(data_no_class)
+
+data_no_class2 = titanic_df.groupby('Pclass')['Pclass'].count()
+print(data_no_class2)
+
+print(titanic_df.sort_values(by='Age'))
+
+#Operations on text data in a data frame
+#Changing all names to lowercase
+titanic_df['Lower'] = titanic_df['Name'].str.lower()
+print(titanic_df['Lower'])
+
+#In Lower field splitting the data 
+
+'''splt = titanic_df['Lower'].str.split(",")
+print(splt)'''
+print(titanic_df['Name'].str.split(" "))
+titanic_df['First Name'] = titanic_df['Name'].str.split(" ").str.get(1)
+print(titanic_df['First Name'])
+titanic_df['Last Name'] = titanic_df['Name'].str.split(" ").str.get(3)
+print(titanic_df['Last Name'])
